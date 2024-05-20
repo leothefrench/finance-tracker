53a110330e18baa2b6c5157eca4790b8bb4d249d
Léandro DA SILVA <leothefrench@gmail.com>
2024-05-20 11:49:11 +0200

    mise en place du framework Hono pour le backend

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,29 @@
-import { SignIn } from "@clerk/nextjs";
+import Image from 'next/image'
+import { Loader2 } from "lucide-react";
+import { SignIn, ClerkLoaded, ClerkLoading } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignIn path="/sign-in" />;
+  return (
+    <div className="min-h-screen grid grid-cols-1 lg:grid-cols-2">
+      <div className="h-full flex flex-col items-center justify-center px-4">
+        <div className="text-center space-y-4 mt-16">
+          <h1 className="text-bold text-3xl text-[#2E2A47]">Welcome back!</h1>
+          <p className="text-base text-[#7E8CA0]">
+            Log in or Create account to get back to your dashboard !
+          </p>
+        </div>
+        <div className="flex justify-center items-center mt-8">
+          <ClerkLoaded>
+            <SignIn path="/sign-in" />
+          </ClerkLoaded>
+          <ClerkLoading>
+            <Loader2  className="animate-spin text-muted-foreground"/>
+          </ClerkLoading>
+        </div>
+      </div>
+      <div className="h-full hidden bg-teal-500 lg:flex items-center justify-center">
+        <Image src='/logo.svg' width={100} height={100} alt='logo' />
+      </div>
+    </div>
+  );
 }
